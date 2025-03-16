a897ba855d702bd62c82f62f855468788247de99
v4lue4dded@gmail.com <v4lue4dded@gmail.com>
2025-03-16 13:30:03 +0100

    found like this 2025-03-16

--- s09_create_basis_for_prepfilter.py
+++ s09_create_basis_for_prepfilter.py
@@ -3,7 +3,6 @@ import json
 import pandas as pd
 import numpy as np
 from sqlalchemy import create_engine
-import math
 import hashlib
 
 # Setup path and configurations
@@ -12,7 +11,10 @@ os.chdir(path.split('GBD', 1)[0] + 'GBD')
 import my_config as config
 
 # Database connection
-engine = create_engine('postgresql://' + config.db_login["user"] + ':' + config.db_login["pw"] + '@localhost:5432/gbd')
+engine = create_engine('postgresql://{user}:{pw}@localhost:5432/gbd'.format(
+    user=config.db_login["user"],
+    pw=config.db_login["pw"]
+))
 con = engine.connect()
 
 # Set export directory
@@ -21,16 +23,16 @@ export_dir = 'test_prepfilter'
 # Query the population data
 df_population = pd.read_sql('''
     SELECT
-        coalesce(year::varchar          , 'All') as year,
-        coalesce(region_name            , 'All') as region_name,
-        coalesce(sub_region_name        , 'All') as sub_region_name,
-        coalesce(location_name          , 'All') as location_name,
-        coalesce(age_group_name_sorted  , 'All') as age_group_name_sorted,
-        coalesce(age_cluster_name_sorted, 'All') as age_cluster_name_sorted,
-        coalesce(sex_name               , 'All') as sex_name,
-        sum(pop_val) as pop_val,
-        sum(pop_upper) as pop_upper,
-        sum(pop_lower) as pop_lower
+        COALESCE(year::varchar, 'All') AS year,
+        COALESCE(region_name, 'All') AS region_name,
+        COALESCE(sub_region_name, 'All') AS sub_region_name,
+        COALESCE(location_name, 'All') AS location_name,
+        COALESCE(age_group_name_sorted, 'All') AS age_group_name_sorted,
+        COALESCE(age_cluster_name_sorted, 'All') AS age_cluster_name_sorted,
+        COALESCE(sex_name, 'All') AS sex_name,
+        SUM(pop_val) AS pop_val,
+        SUM(pop_upper) AS pop_upper,
+        SUM(pop_lower) AS pop_lower
     FROM gbd.db04_modelling.export_population
     GROUP BY ROLLUP(year::varchar, region_name, sub_region_name, location_name, age_group_name_sorted, age_cluster_name_sorted, sex_name)
 ''', con=engine)
@@ -52,37 +54,34 @@ aggregated_columns = [
     'pop_lower',
 ]
 
-# Create dict with all possible categories for each column
-category_dict = {}
+# Optimize by using groupby instead of nested loops
 for i in index_cols:
-    category_dict[i] = df_population[i].unique().tolist()
+    print(i)
+    other_cols = [col for col in index_cols if col != i]
+    grouped = df_population.groupby(other_cols)
 
-# For each column, create a list of all possible combinations of the other columns
-for i in index_cols:
-    # Create a list of all possible combinations of the other columns
-    other_cols = [x for x in index_cols if x != i]
-    combinations = df_population[other_cols].drop_duplicates().to_dict(orient='records')
+    # Create the directory if it doesn't exist
+    directory = os.path.join(export_dir, i)
+    os.makedirs(directory, exist_ok=True)
 
-    # Iterate over each combination
-    for combination in combinations:
-        # Filter df_population to rows matching the combination
-        df_filtered = df_population.copy()
-        for col in other_cols:
-            df_filtered = df_filtered[df_filtered[col] == combination[col]]
+    for combination_values, group in grouped:
+        print(combination_values)
+        # print(group)
+        # Create a combination dict for hashing
+        if len(other_cols) == 1:
+            combination = {other_cols[0]: combination_values}
+        else:
+            combination = dict(zip(other_cols, combination_values))
 
         # Convert grouped DataFrame to JSON
+        json_data = group.set_index(i)[aggregated_columns].to_dict(orient='index')
 
-        json_data = df_filtered.set_index(i)[aggregated_columns].to_dict(orient='index')
+        json_data['generating_combination'] = combination
 
         # Generate a hash for the filename based on the combination
         combination_str = json.dumps(combination, sort_keys=True)
         hash_object = hashlib.md5(combination_str.encode())
         filename = hash_object.hexdigest() + '.json'
-
-        # Create the directory if it doesn't exist
-        directory = os.path.join(export_dir, i)
-        if not os.path.exists(directory):
-            os.makedirs(directory)
 
         filepath = os.path.join(directory, filename)
 
