431b3c3886b069e494dce24eef47315be3036cb8
v4lue4dded@gmail.com <v4lue4dded@gmail.com>
2025-06-05 17:15:59 +0200

    blub

--- s02_import_codebook.py
+++ s02_import_codebook.py
@@ -1,25 +1,27 @@
+# -*- coding: utf-8 -*-
 from os import listdir
-import os
 import my_config as config
-import json
 import pandas as pd
-from zipfile import ZipFile
-import urllib
 from os.path import join as opj
 
+con = config.duckdb_con
 
-engine = config.engine
-con = engine.connect()
-
-codebook_dir =  opj(config.REPO_DIRECTORY,'GBD_2021_DATA_TOOLS_GUIDE')
-
-listdir(codebook_dir)
+codebook_dir = opj(config.REPO_DIRECTORY, "GBD_2021_DATA_TOOLS_GUIDE")
 
 for i_cb in listdir(codebook_dir):
     print(i_cb)
-    if i_cb[-4:] == '.csv':
-        cb_name = 'info_' + i_cb[:-4].lower()
+    if i_cb.endswith(".csv"):
+        cb_name = "info_" + i_cb[:-4].lower()
         print(cb_name)
         df = pd.read_csv(opj(codebook_dir, i_cb))
-        df.columns = df.columns.str.strip().str.lower().str.replace(' ', '_')
-        df.to_sql(name=cb_name, con=engine, schema='db01_import', if_exists='replace', index=False, chunksize=10000)
+        df.columns = df.columns.str.strip().str.lower().str.replace(" ", "_")
+        con.register("df_tmp", df)
+        con.execute(
+            f"""
+            CREATE OR REPLACE TABLE db01_import.{cb_name} AS
+            SELECT * FROM df_tmp;
+        """
+        )
+        con.unregister("df_tmp")
+
+con.close()
