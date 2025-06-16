1121f86fa3ada47cdad5c50a1cfa48d017d4feb3
v4lue4dded@gmail.com <v4lue4dded@gmail.com>
2025-06-16 18:10:49 +0200

    blub

--- s10_export_cachefilter_from_sql.py
+++ s10_export_cachefilter_from_sql.py
@@ -2,6 +2,7 @@ import os
 from os.path import join as opj
 import json
 import my_config as config
+import time
 
 con = config.duckdb_con
 
@@ -28,7 +29,9 @@ FROM (
 """
 
 print(merge_query)
+merge_start = time.time()
 con.execute(merge_query)
+print(f"merged tables in {time.time() - merge_start:.2f} seconds")
 print("Merge table created.")
 
 export_dir = opj(config.REPO_DIRECTORY, "docs", "data_doc", "cachefilter_hash_db_2")
@@ -44,13 +47,15 @@ for (fid,) in prefix_rows:
     file_where[fid] = f"left(identifying_string_hash, 3) = '{fid}'"
 
 file_where["priority_1"] = "priority <= 1"
-file_where["priority_2"] = "priority <= 2"
+file_where["priority_2"] = "priority = 2"
+file_where["priority_3"] = "priority = 3"
 
 metadata = {}
 items = list(file_where.items())
 
 for file_id, where_clause in file_where.items():
     print(file_id)
+    fetch_start = time.time()
     chunk_json_string = con.execute(
         f"""
         SELECT
@@ -60,10 +65,13 @@ for file_id, where_clause in file_where.items():
         WHERE {where_clause}
         """
     ).fetchone()[0]
+    print(f"Fetched dataframe in {time.time() - fetch_start:.2f} seconds")
 
+    write_start = time.time()
     filepath = opj(export_dir, f"{file_id}.json")
     with open(filepath, "w") as f:
         f.write(chunk_json_string)
+    print(f"Written dataframe in {time.time() - write_start:.2f} seconds")
 
 metadata_path = opj(export_dir, "file_sizes.json")
 with open(metadata_path, "w") as meta_file:
